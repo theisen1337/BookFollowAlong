faaf235e4ede7a2c0a7d082f0c587ec13e30bd8f
Thomas Theis <16194561+theisen1337@users.noreply.github.com>
2022-03-19 15:40:51 -0400

    working simple tcp client/server

--- Chapter2/TCPClient.py
+++ Chapter2/TCPClient.py
@@ -6,38 +6,25 @@
 
 """
 
+import socket
 
-
-
-
-
-
-
-
-
-
-
+HOST = "localhost"
+PORT = 9999
+DATA = "Medium pizza extra cheese."     # 25 bytes
 
 def simple_tcp_client():
-    print("Hello World")
-    pass
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        # Connect to server and send data.
+        sock.connect((HOST, PORT))
 
+        # Send data encoded, sendall() is dirty lie.
+        sock.send(DATA.encode())
 
+        # Return data from the server and shut down
+        received = str(sock.recv(1024), "utf-8")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print("\n\nSent from client:  {}".format(DATA))
+    print("Received from server:  {}".format(received))
 
 
 if __name__ == '__main__':
